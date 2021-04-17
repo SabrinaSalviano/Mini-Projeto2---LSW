5f8af74c795dbac0351ebda84438dfd378b523e7
Matheus Valença <82548455+Matheus351@users.noreply.github.com>
2021-04-17 02:03:59 -0300

    Update script.js
    criando função de validação da senha

--- projetoValidadorFormulario/script.js
+++ projetoValidadorFormulario/script.js
@@ -18,3 +18,30 @@ function isName(input){
         divMensagem.style.color = "red"
    }
 }
+function senha(input){
+    let valor = input.value
+    let senhaFormato = /[A-Z\d!|@|#|$|%|^|&|*|(|)|-|_]/gi
+    let teste = senhaFormato.test(valor)
+    if(valor.length==0){
+       let divMensagem = document.getElementById("msgSenha") 
+       divMensagem.innerHTML="Este campo não pode ser vazio" 
+       divMensagem.style.color="red"                          
+   }
+    if(valor.length<4&&valor.length!=0){
+       let divMensagem = document.getElementById("msgSenha")  
+       divMensagem.innerHTML="A senha precisa de no mínimo 4 dígitos"
+       divMensagem.style.color="red"
+    }
+    else{
+       if(teste==true){                                        
+       let divMensagem =  document.getElementById("msgSenha") 
+       divMensagem.innerHTML = "Senha Válida"             
+       divMensagem.style.color = "green"
+    }
+       if(teste==false&&valor.length!=0){
+       let divMensagem =  document.getElementById("msgSenha")
+       divMensagem.innerHTML = "Senha inválida"
+       divMensagem.style.color = "red"
+    }
+   }
+}
